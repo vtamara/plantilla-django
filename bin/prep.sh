bef72e647d3a0e8b0af74d7635860eccd88b23aa
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2013-11-07 17:03:57 -0500

    Imagenes

---
+++ bin/prep.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Prepara sistema para generar con Jekyll
+# Dominio público de acuerdo a legislacion colombiana. 2013
+# vtamara@pasosdeJesus.org
+
+sudo apt-get install ruby irb rubygems 
+sudo gem instal jekyll
